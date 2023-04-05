e8adf025d86be0308e0bfb0a9d5aa14b40fbb5f0
Ross Davidson <50166106+Ross-Davidson-HW@users.noreply.github.com>
2023-04-05 21:59:58 +0100

    Updated pathfinding prototype

--- Mansion Prototype/Mansion Prototype/Assets/Scripts/ThirdPersonController.cs
+++ Mansion Prototype/Mansion Prototype/Assets/Scripts/ThirdPersonController.cs
@@ -141,7 +141,9 @@ namespace StarterAssets
             _input = GetComponent<StarterAssetsInputs>();
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
             _playerInput = GetComponent<PlayerInput>();
+            Cursor.lockState = CursorLockMode.Locked;
 #else
+            
 			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
 #endif
 
